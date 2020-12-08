50049ccc7562178f08270a141f709ce33b7a1235
David Chang <davidchang@Davids-MacBook-Pro.local>
2020-12-08 08:56:23 +0100

    Added Wagon tests

--- UnitTests/WagonTest.cs
+++ UnitTests/WagonTest.cs
@@ -1,10 +1,89 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CircusTrein;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace UnitTests
+namespace Circustrein.Tests
 {
-    class WagonTest
+    [TestClass()]
+    public class WagonTests
     {
+        [TestMethod()]
+        public void AddAnimal_1BigCarnivore1BigHerbivore_IsFalse()
+        {
+            // Arrange
+            Wagon wagon = new Wagon();
+            wagon.IsAnimalAddable(new Animal("", AnimalSize.Large, AnimalFood.Carnivore));
+
+            // Act
+            bool isAdded = wagon.IsAnimalAddable(new Animal("", AnimalSize.Large, AnimalFood.Herbivore));
+
+            //Assert
+            Assert.IsFalse(isAdded);
+        }
+
+        [TestMethod()]
+        public void AddAnimal_2SmallCarnivore1BigHerbivore_IsTrue()
+        {
+            // Arrange
+            Wagon wagon = new Wagon();
+            wagon.IsAnimalAddable(new Animal("", AnimalSize.Small, AnimalFood.Carnivore));
+            wagon.IsAnimalAddable(new Animal("", AnimalSize.Small, AnimalFood.Carnivore));
+
+            // Act
+            bool isAdded = wagon.IsAnimalAddable(new Animal("",AnimalSize.Large, AnimalFood.Herbivore));
+
+            //Assert
+            Assert.IsTrue(isAdded);
+        }
+
+        [TestMethod()]
+        public void AddAnimal_3MediumCarnivore1SmallHerbivore_IsFalse()
+        {
+            // Arrange
+            Wagon wagon = new Wagon();
+            wagon.IsAnimalAddable(new Animal("", AnimalSize.Medium, AnimalFood.Carnivore));
+            wagon.IsAnimalAddable(new Animal("", AnimalSize.Medium, AnimalFood.Carnivore));
+            wagon.IsAnimalAddable(new Animal("", AnimalSize.Medium, AnimalFood.Carnivore));
+
+            // Act
+            bool isAdded = wagon.IsAnimalAddable(new Animal("", AnimalSize.Small, AnimalFood.Herbivore));
+
+            //Assert
+            Assert.IsFalse(isAdded);
+        }
+
+        [TestMethod()]
+        public void AddAnimal_3MediumHerbivore1SmallHerbivore_IsTrue()
+        {
+            // Arrange
+            Wagon wagon = new Wagon();
+            wagon.IsAnimalAddable(new Animal("", AnimalSize.Medium, AnimalFood.Herbivore));
+            wagon.IsAnimalAddable(new Animal("", AnimalSize.Medium, AnimalFood.Herbivore));
+            wagon.IsAnimalAddable(new Animal("", AnimalSize.Medium, AnimalFood.Herbivore));
+
+            // Act
+            bool isAdded = wagon.IsAnimalAddable(new Animal("", AnimalSize.Small, AnimalFood.Herbivore));
+
+            //Assert
+            Assert.IsTrue(isAdded);
+        }
+
+        [TestMethod()]
+        public void AddAnimal_3MediumHerbivore1MediumHerbivore_IsFalse()
+        {
+            // Arrange
+            Wagon wagon = new Wagon();
+            wagon.IsAnimalAddable(new Animal("", AnimalSize.Medium, AnimalFood.Herbivore));
+            wagon.IsAnimalAddable(new Animal("", AnimalSize.Medium, AnimalFood.Herbivore));
+            wagon.IsAnimalAddable(new Animal("", AnimalSize.Medium, AnimalFood.Herbivore));
+
+            // Act
+            bool isAdded = wagon.IsAnimalAddable(new Animal("", AnimalSize.Medium, AnimalFood.Herbivore));
+
+            //Assert
+            Assert.IsFalse(isAdded);
+        }
     }
 }
